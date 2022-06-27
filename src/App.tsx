18428264e141028479069689cafaac26cc180cc0
Tim Rooke <timrooke1991@gmail.com>
2022-06-27 11:15:11 +0100

    Initial project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import { AppHeader } from './components/AppHeader';
+
+
+function App() {
+  return (
+    <div className="App">
+      <AppHeader title="Rock, Paper, Scissors" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AppHeader.tsx
@@ -0,0 +1,17 @@
+
+import React, { FunctionComponent } from 'react'; // we need this to make JSX compile
+
+interface AppHeaderProps {
+  title: string
+}
+
+export const AppHeader: FunctionComponent<AppHeaderProps> = ({ title}) => {
+  // split on the
+  return (
+    <div className="Column">
+      {
+        title.split(', ').map((word, index) => <h1 className="App-header" key={`${word}-${index}`}>{word}</h1>)
+      }
+    </div>
+  )
+}
